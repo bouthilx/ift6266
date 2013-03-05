d8992ce3092f61f6bf3b9cff376ce4ffe68e531d
gbcolborne <g.b.colborne@gmail.com>
2013-03-05 10:29:39 -0500

    Made corrections in dispatch.py

--- dispatch.py
+++ dispatch.py
@@ -6,15 +6,14 @@ from pylearn2.utils.shell import run_shell_command
 # jobdispatch /data/lisatmp/username/my_super_script.py 
 # /data/lisatmp/ift6266h13/${USER}/conﬁg_ﬁle”{{1,2,3}}”.yaml
 
-expdir = '/data/lisatmp'
+expdir = '/data/lisatmp/ift6266h13/berniergtmp/ift6266/experiment1'
 names = os.listdir(expdir)
-dirs = [expdir + '/' + name for name in names]
-dirs = [d for d in dirs if not os.path.exists(d + '/cluster_info.txt')]
+names = [expdir + '/' + name for name in names if name[-5:] == '.yaml']
 command = 'jobdispatch --torque --
 env=THEANO_FLAGS=device=gpu,ﬂoatX=ﬂoat32,force_device=True' + \
         ' --duree=48:00:00 --whitespace ' + \
-        '--gpu bash /RQexec/goodfell/galatea/mlp/experiment_6/worker.sh '
+        '--gpu python /data/lisatmp/ift6266h13/berniergtmp/ift6266/train.py '
 command += ' "{{'
-command += ', '.join(dirs)
+command += ', '.join(names)
 command += '}}" '
 output, rc = run_shell_command(command)
